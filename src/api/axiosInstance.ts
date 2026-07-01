35a75dd1db1aab2b28a2882e4224cf26fe9b8746
aleja <alejandrovasquezesc@gmail.com>
2026-07-01 14:04:16 -0600

    feat(api): use VITE_API_URL env variable as axios base URL

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-
 const apiInstance = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 apiInstance.interceptors.request.use((config) => {
